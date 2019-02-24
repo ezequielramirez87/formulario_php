ceb3f03c453c447fab5bc6e5b43b8abf250601ba
ezequielramirez87 <38512915+ezequielramirez87@users.noreply.github.com>
2019-02-24 20:45:23 -0300

    first commit

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+    $destino="ezequiel.ramirez.87@gmail.com";
+    $nombre=$_POST["nombre"];
+    $correo=$_POST["correo"];
+    $telefono=$_POST["telefono"];
+    $mensaje=$_POST["mensaje"];
+    $contenido="Nombre".$nombre."\nCorre: ".$correo."\nTelefono: ".$telefono."\nMensaje: ".$mensaje;
+    mail($destino,"Contacto",$contenido);
+    header("location:gracias.html")
+?>
